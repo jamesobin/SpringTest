097fda900cd4846000f864d4e3cec20808a37875
jamesobin <jamesobin@naver.com>
2024-11-01 18:14:02 +0900

    [권오빈]  -hello world

---
+++ src/main/java/com/jamesobin/spring/test/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.jamesobin.spring.test.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @ResponseBody
+    @RequestMapping("/helloTest")
+    public String helloWorld() {
+        return "Hello World!!";
+    }
+}
